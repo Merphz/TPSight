e5f5733cc7887a093da997f55ff6f6dda06011d0
Merphz <jari.jaaskela77@gmail.com>
2016-07-09 16:54:24 +0300

    commit

--- scanner.js
+++ scanner.js
@@ -11,18 +11,29 @@ const lightScanInterval = 60 * 1000 * 60; //every hour
 class Scanner {
   constructor() {
     this.itemWatchers = [];
+    this.currentIndex = 0;
+    this.itemCount = 0;
   }
 
   start() {
+    this.currentIndex = 0;
     this.itemWatchers = [];
+
     tp.getItemIds((chunk) => {
       const items = JSON.parse(chunk);
+      this.itemCount = items.length;
 
       for (const itemId of items) {
         //console.log('Registering item watcher for id ' + itemId);
         let watcher = new ItemWatcher(itemId);
         this.itemWatchers.push(watcher);
         watcher.start(lightScanInterval);
+        watcher.on('listingsFetched', () => {
+          //console.log('Fetched for item ' + itemId);
+          this.currentIndex++;
+          console.log('Progress: ' + (this.currentIndex / this.itemCount) * 100 + ' %');
+        });
+
       }
     });
     }
--- watchers/item_watcher.js
+++ watchers/item_watcher.js
@@ -14,69 +14,55 @@ const hash = require('json-hash');
 //const connString = 'mongodb://Aenox:D7vm4vv6@ds028559.mlab.com:28559/gw2interdb';
 const connString = 'mongodb://Aenox:D7vm4vv6@46.101.229.200:27017/gw2tp';
 
+let dbConn = null;
+
+mongo.connect(connString, function(err, db) {
+  assert.equal(err, null);
+  dbConn = db;
+});
 
 var addListings = function(itemId, listings) {
-  mongo.connect(connString, function(err, db) {
-    //assert.equal(err, null);
+    if (dbConn) {
+      dbConn.collection('tp_items', (err, collection) => {
+        assert.equal(err, null);
 
-    if (err != null) {
-      console.log('Could not connect to the db server');
-      return;
-    }
+        const listingHash = hash.digest(listings);
 
-    db.collection('tp_items', (err, collection) => {
-
-      assert.equal(err, null);
-
-      const listingHash = hash.digest(listings);
-
-      var cursor = collection.find(
-          {
-            'itemId': {$eq: itemId},
-            'latestHash': {$eq: listingHash}
-          }
-      );
-
+        var cursor = collection.find(
+            {
+              'itemId': {$eq: itemId},
+              'latestHash': {$eq: listingHash}
+            }
+        );
       cursor.limit(1).next(function (err, result) {
         assert.equal(err, null);
 
         // If does not exist
         if (!result) {
           try {
-            //console.log('db push item: ' + itemId);
-             /*Push listings to the db. If itemId does not exists it creates a new object.*/
+            console.log('db push item: ' + itemId);
+            /*Push listings to the db. If itemId does not exists it creates a new object.*/
             collection.updateOne(
-                { 'itemId': itemId },
-                { $set: { latestHash: listingHash },
-                  $push: { 'listings': {
-                    timestamp: new Date().toISOString(), 'listings': listings, 'hash': listingHash} }
+                {'itemId': itemId},
+                {
+                  $set: {latestHash: listingHash},
+                  $push: {
+                    'listings': {
+                      timestamp: new Date().toISOString(), 'listings': listings, 'hash': listingHash
+                    }
+                  }
                 },
-                { upsert: true }
+                {upsert: true}
             );
           } catch (e) {
             print(e);
           }
         }
-        db.close();
+        });
       });
-
-
-      if (cursor.count() > 0) {
-        // Listings already exists
-        console.log('already exists');
-        console.log('cursor count: ' + cursor.count());
-        db.close();
-        return;
-      }
-
-      /*console.log('db push item: ' + itemId);
-      /*Push listings to the db. If itemId does not exists it creates a new object.*/
-
-      /*
-
-      db.close();*/
-    });
-  });
+    } else {
+      console.log('Database connection is dead');
+    }
 };
 
 class ItemWatcher extends Watcher {
@@ -93,6 +79,11 @@ class ItemWatcher extends Watcher {
           console.dir(item);
           return;
         }
+
+        if (this.events['listingsFetched']) {
+          this.events['listingsFetched']();
+        }
+
         //console.dir(item);
         addListings(item.id, {buys: item.buys, sells: item.sells});
       });
